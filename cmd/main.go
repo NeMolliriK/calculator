d7ab2756d4544750a512918c9e0f3cd1952a115c
NeMo lliriK <nemollirik@gmail.com>
2024-12-22 22:46:37 +0500

    Add .env

--- cmd/main.go
+++ cmd/main.go
@@ -2,19 +2,43 @@ package main
 
 import (
 	"calculator/internal/application"
+	"fmt"
+	"github.com/joho/godotenv"
 	"log/slog"
 	"os"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Warning: .env file not found, falling back to system environment variables")
+	}
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	logLevel := os.Getenv("LOG_LEVEL")
+	var level slog.Level
+	switch logLevel {
+	case "DEBUG":
+		level = slog.LevelDebug
+	case "INFO":
+		level = slog.LevelInfo
+	case "WARN":
+		level = slog.LevelWarn
+	case "ERROR":
+		level = slog.LevelError
+	default:
+		level = slog.LevelInfo
+	}
 	logFile, err := os.OpenFile("logs.txt", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 	if err != nil {
 		panic(err)
 	}
 	defer logFile.Close()
-	logger := slog.New(slog.NewTextHandler(logFile, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	logger := slog.New(slog.NewTextHandler(logFile, &slog.HandlerOptions{Level: level}))
 	app := application.New(logger)
-	if err := app.Run(); err != nil {
+	if err := app.Run(port); err != nil {
 		logger.Error("Application failed to run", "error", err)
 	}
 }
--- internal/application/application.go
+++ internal/application/application.go
@@ -16,14 +16,14 @@ func New(logger *slog.Logger) *Application {
 		logger: logger,
 	}
 }
-func (a *Application) Run() error {
+func (a *Application) Run(port string) error {
 	mux := http.NewServeMux()
 	mux.Handle(
 		"/api/v1/calculate",
 		server.LoggingMiddleware(server.CalculatorHandler(a.logger), a.logger),
 	)
 	fmt.Println("The only endpoint is available at http://localhost:8080/api/v1/calculate")
-	err := http.ListenAndServe(":8080", mux)
+	err := http.ListenAndServe(fmt.Sprintf(":%s", port), mux)
 	if err != nil {
 		a.logger.Error("Failed to start the server", "error", err)
 	}
